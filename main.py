2a646a6164760ee5f1847f616471f0a0b130d681
Lawliet <leelawliet7@gmail.com>
2021-08-18 18:11:25 -0400

    play video at normal speed

--- main.py
+++ main.py
@@ -1,72 +1,11 @@
 import sys
+import time
+
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, QObject, pyqtSignal
 import cv2
 import imutils
 
-
-# class _Bar(QtWidgets.QWidget):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#
-#         self.setSizePolicy(
-#             QtWidgets.QSizePolicy.MinimumExpanding,
-#             QtWidgets.QSizePolicy.MinimumExpanding
-#         )
-#
-#
-#
-#     def sizeHint(self):
-#         return QtCore.QSize(40, 120)
-#
-#     def paintEvent(self, e):
-#         painter = QtGui.QPainter(self)
-#
-#         brush = QtGui.QBrush()
-#         brush.setColor(QtGui.QColor('black'))
-#         brush.setStyle(Qt.SolidPattern)
-#         rect = QtCore.QRect(0, 0, painter.device().width(), painter.device().height())
-#         painter.fillRect(rect, brush)
-#
-#         # Get current state.
-#         dial = self.parent()._dial
-#         vmin, vmax = dial.minimum(), dial.maximum()
-#         value = dial.value()
-#
-#         pen = painter.pen()
-#         pen.setColor(QtGui.QColor('red'))
-#         painter.setPen(pen)
-#
-#         font = painter.font()
-#         font.setFamily('Times')
-#         font.setPointSize(18)
-#         painter.setFont(font)
-#
-#         painter.drawText(25, 25, "{}-->{}<--{}".format(vmin, value, vmax))
-#         painter.end()
-#
-#     def _trigger_refresh(self):
-#         self.update()
-#
-# class PowerBar(QtWidgets.QWidget):
-#     """
-#     Custom Qt Widget to show a power bar and dial.
-#     Demonstrating compound and custom-drawn widget.
-#     """
-#
-#     def __init__(self, steps=5, *args, **kwargs):
-#         super(PowerBar, self).__init__(*args, **kwargs)
-#
-#         layout = QtWidgets.QVBoxLayout()
-#         self._bar = _Bar()
-#         layout.addWidget(self._bar)
-#
-#         self._dial = QtWidgets.QDial()
-#         layout.addWidget(self._dial)
-#
-#         self.setLayout(layout)
-#
-#         self._dial.valueChanged.connect(self._bar._trigger_refresh)
 from PyQt5.QtGui import QImage
 
 
@@ -106,6 +45,24 @@ def scale_image(img, scale):
     return cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
 
+def num_frames_to_num_millis(num_frames):
+    return int(round(1000 / num_frames))
+
+
+def get_perf_counter_as_millis():
+    return int(time.perf_counter() * 1000)
+
+
+def skip_until_frame(cap, num_frames):
+    for i in range(num_frames):
+        frame = get_next_frame(cap)
+    return frame
+
+
+class Communicate(QObject):
+    update_frame = pyqtSignal()
+
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, video):
         super().__init__()
@@ -113,16 +70,30 @@ class MainWindow(QtWidgets.QMainWindow):
         self.cap = cv2.VideoCapture(video)
         print(print_all_populated_openv_attrs(self.cap))
         width, height = get_video_dimensions(self.cap)
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
 
         self.label = QtWidgets.QLabel()
         # canvas = QtGui.QPixmap(800, 600)
         # self.label.setPixmap(canvas)
-        frame = get_next_frame(self.cap, specific_frame=20000)
+        frame = get_next_frame(self.cap, specific_frame=10000)
         # scaled_frame = resize(frame, (800, 600))
         scaled_frame = imutils.resize(frame, width=1024)
         self.label.setPixmap(QtGui.QPixmap(convert_cvimg_to_qimg(scaled_frame)))
         self.setCentralWidget(self.label)
         self.draw_something()
+
+        self.current_time = int(time.perf_counter() * 1000)
+        print(self.current_time)
+        # print(time.perf_counter())
+
+        self.c = Communicate()
+        self.c.update_frame.connect(self.update_frame)
+
+        self.timer = QTimer(self)
+        self.timer.setInterval(num_frames_to_num_millis(self.fps))
+        self.timer.setTimerType(QtCore.Qt.PreciseTimer)
+        self.timer.timeout.connect(self.emit_update_frame_signal)
+        self.timer.start()
 
     def draw_something(self):
         painter = QtGui.QPainter(self.label.pixmap())
@@ -132,6 +103,21 @@ class MainWindow(QtWidgets.QMainWindow):
         painter.setPen(pen)
         painter.drawLine(10, 10, 300, 200)
         painter.end()
+
+    def update_frame(self):
+        # print("updating frame")
+        # print(time.perf_counter())
+        new_time = get_perf_counter_as_millis()
+        time_diff = new_time - self.current_time
+        frame_diff = int(round(time_diff/self.fps))
+        frame = skip_until_frame(self.cap, frame_diff)
+        scaled_frame = imutils.resize(frame, width=1024)
+        self.label.setPixmap(QtGui.QPixmap(convert_cvimg_to_qimg(scaled_frame)))
+        self.current_time = new_time
+        # self.update()
+
+    def emit_update_frame_signal(self):
+        self.c.update_frame.emit()
 
 
 if __name__ == '__main__':

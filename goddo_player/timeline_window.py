60d69339c4001386ba3e36cf989cfd0fc4862273
Lawliet <leelawliet7@gmail.com>
2021-09-02 15:18:20 -0400

    timeline with scrollbar

--- goddo_player/timeline_window.py
+++ goddo_player/timeline_window.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtGui
 from PyQt5.QtGui import QMouseEvent
 from PyQt5.QtWidgets import QVBoxLayout, QLabel
 
@@ -31,9 +31,33 @@ class TimelineWindow(QWidget):
         # layout.addWidget(self.label)
         self.setLayout(layout)
 
+        self.scrolling = False
+        self.scrollbar_rect: QRect = None
+        self.scrollbar_prev_pos: QPoint = None
+        self.scrollbar_width = 40
+        self.scrollbar_height = 15
+        self.scrollbar_min_x = 2
+        self.scrollbar_speed = 10
+        self.scrollbar_max_x = self.geometry().width() - self.scrollbar_min_x - self.scrollbar_width
+
+        self.grid_offset = 0
+        self.scrollbar_x_offset = 0
+
+    def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
+        if event.key() == Qt.Key_Left:
+            self.scrollbar_x_offset = self.scrollbar_x_offset - 1
+            print(f"left {self.scrollbar_x_offset}")
+            self.update()
+        elif event.key() == Qt.Key_Right:
+            self.scrollbar_x_offset = self.scrollbar_x_offset + 1
+            print(f"right {self.scrollbar_x_offset}")
+            self.update()
+        else:
+            super().keyPressEvent(event)
+
     def paintEvent(self, e):
-        print('painting')
         painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
 
         brush = create_fill_in_brush(QColor(51, 51, 51))
         rect = QtCore.QRect(0, 0, painter.device().width(), painter
@@ -49,47 +73,69 @@ class TimelineWindow(QWidget):
         height = 30
         painter.drawLine(0, height, self.geometry().width(), height)
 
-        offset = 0
         tick_spacing = 10
         tick_height = 10
-        num_of_ticks = int(self.geometry().width() / tick_spacing)
-        for i in range(num_of_ticks):
-            x = i*tick_spacing+offset
-            if i % 10 == 0:
-                cur_tick_height = height - tick_height * 1.5
-            elif i % 5 == 0:
-                cur_tick_height = height - tick_height
-            else:
-                cur_tick_height = height - tick_height / 2
-            painter.setPen(regular_pen)
-            painter.drawLine(x, height, x, cur_tick_height)
-
-            if i % 10 == 0:
-                painter.setPen(half_opacity_pen)
-                painter.drawLine(x, height, x, self.geometry().height())
-
+        # num_of_ticks = int(self.geometry().width() / tick_spacing)
+        # print(num_of_ticks)
+        i = 0
+        x = 0
+        while x < self.geometry().width():
+            x = i*tick_spacing+self.scrollbar_x_offset*self.scrollbar_speed*-1
+            # print(x)
+            # if x < 0:
+            #     continue
+            if x >= 0:
+                if i % 10 == 0:
+                    cur_tick_height = height - tick_height * 1.5
+                elif i % 5 == 0:
+                    cur_tick_height = height - tick_height
+                else:
+                    cur_tick_height = height - tick_height / 2
                 painter.setPen(regular_pen)
-                time_str = format_time(*ms_to_time_components(1000*60*i/10, 30))
-                painter.drawText(QPoint(x, 10), time_str)
+                painter.drawLine(x, height, x, cur_tick_height)
 
-        scrollbar_height = 15
+                if i % 10 == 0:
+                    painter.setPen(half_opacity_pen)
+                    painter.drawLine(x, height, x, self.geometry().height())
+
+                    painter.setPen(regular_pen)
+                    time_str = format_time(*ms_to_time_components(1000*60*i/10, 30))
+                    painter.drawText(QPoint(x, 10), time_str)
+
+            i = i + 1
+
         painter.setPen(regular_pen)
-        painter.drawRect(QRect(0, self.geometry().height()-scrollbar_height, self.geometry().width(), scrollbar_height))
+        painter.drawRect(QRect(0, self.geometry().height()-self.scrollbar_height,
+                               self.geometry().width(), self.scrollbar_height))
 
+        x = min(self.scrollbar_max_x, max(self.scrollbar_min_x, self.scrollbar_min_x+self.scrollbar_x_offset))
+        self.scrollbar_rect = QRect(x, self.geometry().height() - self.scrollbar_height+4,
+                                    self.scrollbar_width, self.scrollbar_height-8)
         painter.setBrush(create_fill_in_brush(Qt.white))
-        painter.drawRoundedRect(QRect(2, self.geometry().height() - scrollbar_height+2, 100, scrollbar_height-4), 5, 5)
+        painter.drawRoundedRect(self.scrollbar_rect, 3, 3)
 
         print(self.geometry())
 
     def mousePressEvent(self, event: QMouseEvent):
         print("mouse press")
+        if self.scrollbar_rect.contains(event.pos()):
+            self.scrolling = True
+            self.scrollbar_prev_pos = event.pos()
         # self.update()
+
+    def mouseReleaseEvent(self, event: QMouseEvent):
+        print("mouse release")
+        self.scrolling = False
 
     def mouseMoveEvent(self, event: QMouseEvent):
         print("mouse move")
-        self.update()
+        if self.scrolling:
+            print(event.pos())
+            # self.grid_offset = self.grid_offset - (event.pos().x() - self.scrollbar_prev_pos.x())
+            self.update()
 
     def enterEvent(self, event: QtCore.QEvent):
         self.activateWindow()
+
 
 
